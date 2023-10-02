6b681dece69ee80cb724b9334dfa7ba40cf2c0ee
aaqil <kidobop@gmail.com>
2023-10-02 22:32:15 +0530

    made the game function

--- rockpapersci.js
+++ rockpapersci.js
@@ -6,10 +6,11 @@ function getComputerChoice(){
     return choice[randomChoice];
 }
 
-const playerSelection = prompt("Enter Rock,Paper or Scissors:");
-const playerSelectionLower = playerSelection.toLowerCase();
+const round = prompt("Enter no of rounds:");
+
 
 const computerSelection = getComputerChoice();
+let playerScore=0,computerScore=0;
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection===computerSelection) {
@@ -20,8 +21,22 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection==="scissors" && computerSelection==="paper") ||
         (playerSelection==="paper" && computerSelection==="rock")
     ){
+        playerScore++;
         return `You Won! ${playerSelection} beats ${computerSelection}`;
-    } else return `You Lost!  ${computerSelection} beats ${playerSelection}`;
+    } else {
+        computerScore++;
+        return `You Lost!  ${computerSelection} beats ${playerSelection}`;
+    }
 }
 
-console.log(playRound(playerSelection,computerSelection));
+function game() {
+    for (let index = 0; index <= round; index++) {
+        const playerSelection = prompt("Enter Rock,Paper or Scissors:");
+        const playerSelectionLower = playerSelection.toLowerCase();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log(`Your score is ${playerScore}`);
+}
+
+
+console.log(game());
